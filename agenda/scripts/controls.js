30905c5efab232638efbac2d733e8cd3e51a8aaf
arcan@Rafa <rafael.rued4@gmail.com>
2024-05-17 19:58:40 -0300

    scrollto smoothly

--- agenda/scripts/controls.js
+++ agenda/scripts/controls.js
@@ -16,6 +16,34 @@ document.addEventListener("DOMContentLoaded", function () {
         return Array.from(array, byte => ('0' + byte.toString(16)).slice(-2)).join('').toUpperCase();
     };
 
+    // Function not necessary to comprehend
+    function scrollToSmoothly(scrollTo, duration) {
+        var start = window.scrollY || document.documentElement.scrollTop,
+            change = scrollTo - start,
+            currentTime = 0,
+            increment = 20;
+
+        var animateScroll = function () {
+            currentTime += increment;
+            var val = Math.easeInOutQuad(currentTime, start, change, duration);
+            window.scrollTo(0, val);
+            if (currentTime < duration) {
+                requestAnimationFrame(animateScroll);
+            }
+        };
+
+        animateScroll();
+    }
+
+    // Not necessary to comprehend
+    // Add the easeInOutQuad function to the Math object for smooth interpolation
+    Math.easeInOutQuad = function (t, b, c, d) {
+        t /= d / 2;
+        if (t < 1) return c / 2 * t * t + b;
+        t--;
+        return -c / 2 * (t * (t - 2) - 1) + b;
+    };
+
     let atualField = 0;
     const addButton = document.querySelector('.add-field');
 
@@ -177,6 +205,9 @@ document.addEventListener("DOMContentLoaded", function () {
         });
 
         fieldClicked(document.getElementById(randomCode), atualField + 1);
+
+        const newQuestionTop = questionDiv.getBoundingClientRect().top + window.scrollY;
+        scrollToSmoothly(newQuestionTop, 1000);
 
         // For backend hidden inputs with values
         const questionHiddenInput = document.createElement('input');
