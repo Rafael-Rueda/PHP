78d0d06ef0e51fec2e231b2db0e245d98af0f474
Rafael-Linux <rafael.rued4@gmail.com>
2024-07-01 14:28:10 -0300

    base change

--- agenda/utils/base_url.php
+++ agenda/utils/base_url.php
@@ -1,6 +1,6 @@
 <?php
 // $path = '/github/agenda/';
-$path = '/cursophp/PHP/agenda/';
+$path = '/cursophp/agenda/';
 
 define("BASE_URL", 'http://' . $_SERVER['HTTP_HOST'] . $path);
 define("BASE_PATH", ($_SERVER['DOCUMENT_ROOT'] . $path));
